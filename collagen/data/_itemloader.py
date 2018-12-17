8544b050efa0306b4124bed6004573f1d4f9d790
lext <aleksei.tiulpin@protonmail.ch>
2018-12-17 19:28:49 +0200

    Refactored itemloader

--- collagen/data/_itemloader.py
+++ collagen/data/_itemloader.py
@@ -8,6 +8,8 @@ from torch.utils.data.sampler import SequentialSampler, RandomSampler, BatchSamp
 from torchvision.datasets import DatasetFolder
 from torch.utils.data import Dataset
 from ._dataset import DataFrameDataset
+import numpy as np
+
 
 class ItemLoader(object):
     """ItemLoader. Combines DataFrameDataset and DataLoader, and provides single- or multi-process iterators over the dataset.
@@ -34,7 +36,7 @@ class ItemLoader(object):
     collate_fn : callable, optional
         Merges a list of samples to form a mini-batch. (the default is None)
     transform : callable, optional
-        Tranforms row of :attr:`meta_data` (the default is None)
+        Transforms row of :attr:`meta_data` (the default is None)
     sampler : Sampler, optional
         Defines the strategy to draw samples from
         the dataset. If specified, ``shuffle`` must be False. (the default is None)
@@ -50,54 +52,39 @@ class ItemLoader(object):
     timeout : int, optional
         if positive, the timeout value for collecting a batch
         from workers. Should always be non-negative. (the default is 0)
-    worker_init_fn : [type], optional
-        If not ``None``, this will be called on each
-        worker subprocess with the worker id (an int in ``[0, num_workers - 1]``) as
-        input, after seeding and before data loading. (the default is None)
-    
-    Note
-    ----
-    By default, each worker will have its PyTorch seed set to
-    ``base_seed + worker_id``, where ``base_seed`` is a long generated
-    by main process using its RNG. However, seeds for other libraies
-    may be duplicated upon initializing workers (w.g., NumPy), causing
-    each worker to return identical random numbers. (See
-    :ref:`dataloader-workers-random-seed` section in FAQ.) You may
-    use :func:`torch.initial_seed()` to access the PyTorch seed for
-    each worker in :attr:`worker_init_fn`, and use it to set other
-    seeds before data loading.
-    
     """
 
-    def __init__(self, root, meta_data, parse_item_cb, batch_size=1, num_workers=0, shuffle=False, pin_memory=False, collate_fn=None, transform=None,
-                sampler=None, batch_sampler=None, drop_last=True, timeout=0, worker_init_fn=None):
+    def __init__(self, root: str, meta_data: pd.DataFrame, parse_item_cb: callable, batch_size: int = 1,
+                 num_workers: int = 0, shuffle: bool = False, pin_memory: bool = False, collate_fn: callable = None,
+                 transform: callable or None = None,
+                 sampler: torch.utils.data.sampler.Sampler or None = None,
+                 batch_sampler=None, drop_last: bool = True, timeout: int = 0):
 
-        self.dataset = DataFrameDataset(root, meta_data=meta_data, parse_item_cb=parse_item_cb)
-        self.data_loader = torch.utils.data.DataLoader(self.dataset, 
-                                                        batch_size=batch_size, 
-                                                        shuffle=shuffle,
-                                                        sampler=sampler,
-                                                        batch_sampler=batch_sampler,
-                                                        num_workers=num_workers,
-                                                        # collate_fn=collate_fn,
-                                                        pin_memory=pin_memory,
-                                                        drop_last=drop_last,
-                                                        timeout=timeout,
-                                                        worker_init_fn=worker_init_fn)
+        self.dataset = DataFrameDataset(root, meta_data=meta_data, parse_item_cb=parse_item_cb, transform=transform)
+        self.data_loader = torch.utils.data.DataLoader(self.dataset,
+                                                       batch_size=batch_size,
+                                                       shuffle=shuffle,
+                                                       sampler=sampler,
+                                                       batch_sampler=batch_sampler,
+                                                       num_workers=num_workers,
+                                                       collate_fn=collate_fn,
+                                                       pin_memory=pin_memory,
+                                                       drop_last=drop_last,
+                                                       timeout=timeout,
+                                                       worker_init_fn=lambda wid: np.random.seed(np.uint32(
+                                                           torch.initial_seed() + wid)))
+
         self.iter_batch_sampler = iter(self.data_loader)
         self.drop_last = drop_last
         self.batch_size = batch_size
-
-        if collate_fn is not None:
-            self.data_loader.collate_fn=collate_fn
         
     def __len__(self):
         """ Get length of dataset
         """
         return len(self.dataset)
 
-    def sampler(self, k=1):
-        """Sample a number of batches
+    def sample(self, k=1):
+        """Sample oen or more mini-batches
         
         Parameters
         ----------
