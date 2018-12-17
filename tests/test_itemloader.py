6539dd18113795fc559f6428a2a975e2dbc81e5c
Huy Hoang Nguyen <hunguyen18@univ.yo.oulu.fi>
2018-12-17 16:31:47 +0200

    - Remove creating local files in test

--- tests/test_itemloader.py
+++ tests/test_itemloader.py
@@ -35,38 +35,38 @@ def test_not_enough_arg_input_1_arg():
 def test_loader_same_shape_sampler_k_1_batch_3():
     root = "./imgs_test_loader_sampler_k_1_batch_3"
     batch_size = 3
-    if os.path.exists(root):
-        rmtree(root)
-    os.makedirs(root)
+#     if os.path.exists(root):
+        # rmtree(root)
+#     os.makedirs(root)
     list_imgs = []
     for i in range(9):
-        file_name = "img" + str(i+1) + ".png"
-        file_fullname = os.path.join(root, file_name)
+        # file_name = "img" + str(i+1) + ".png"
+        # file_fullname = os.path.join(root, file_name)
         img = np.zeros((2, 9), dtype=int)
         img[:,i] = 1
         list_imgs.append(np.copy(img))
-        cv2.imwrite(file_fullname, img)
+        # cv2.imwrite(file_fullname, img)
     
     itermloader = ItemLoader(root, meta_data=md_1_df_9_row_y_class(), batch_size=batch_size, parse_item_cb=md_1_parse_item)
     samples = itermloader.sampler(1)
 
     # Prepare result
     # X
-    batched_imgs = np.stack(list_imgs[:batch_size], axis=0).astype(np.int32)
+    # batched_imgs = np.stack(list_imgs[:batch_size], axis=0).astype(np.int32)
     # y
     batched_labels = np.zeros((3, 6), dtype=np.float)
     batched_labels[0,0] = 1
     batched_labels[1,1] = 1
     batched_labels[2,2] = 1
 
-    expected_tensor = torch.from_numpy(batched_imgs)
+    # expected_tensor = torch.from_numpy(batched_imgs)
     expected_target = torch.from_numpy(batched_labels)
 
-    expected_output = {'img': expected_tensor, 'target': expected_target}
+    expected_output = {'target': expected_target}
     # print("Predicted:\n{}\nGroundtruth:\n{}".format(samples[0], expected_output))
     
-    diff_img = torch.sum(torch.abs(samples[0]['img'] - expected_output['img']))
+    # diff_img = torch.sum(torch.abs(samples[0]['img'] - expected_output['img']))
     diff_target = torch.sum(torch.abs(samples[0]['target'] - expected_output['target']))
 
     # print("Diff img: {}, diff target: {}".format(diff_img, diff_target))
-    assert(diff_img == 0 and diff_target == 0)
+    assert(diff_target == 0)
