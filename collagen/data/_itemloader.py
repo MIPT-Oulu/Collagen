75519e29ffc11d691faddd6f5c953df1ebbe1d1b
Egor Panfilov <egor.v.panfilov@gmail.com>
2018-12-19 14:27:01 +0200

    Cosmetics for _itemloader.py

--- collagen/data/_itemloader.py
+++ collagen/data/_itemloader.py
@@ -1,11 +1,10 @@
+import numpy as np
 import pandas as pd
 import torch
 from torch.utils.data.sampler import SequentialSampler
-
 from torch.utils.data.dataloader import default_collate
 
 from ._dataset import DataFrameDataset
-import numpy as np
 
 
 class ItemLoader(object):
@@ -14,41 +13,40 @@ class ItemLoader(object):
     Parameters
     ----------
     root : str
-        Path to root directory of input data
+        Path to root directory of input data.
     meta_data : pandas.DataFrame
-        Meta data of data and labels
+        Meta data of data and labels.
     parse_item_cb : callable
-        Parses each row of :attr:meta_data`
+        Parses each row of :attr:meta_data`.
     batch_size : int, optional
-        How many data per batch to load (the default is 1)
+        How many data per batch to load. (the default is 1)
     num_workers : int, optional
-        How many subprocesses to use for data
-        loading. 0 means that the data will be loaded in the main process. (the default is 0)
+        How many subprocesses to use for data loading. If equals to 0, 
+        the data will be loaded in the main process. (the default is 0)
     shuffle : bool, optional
-        Set to ``True`` to have the data reshuffled
-        at every epoch (the default is False)
+        Set to ``True`` to have the data reshuffled at every epoch. (the default is False)
     pin_memory : bool, optional
-        If ``True``, the data loader will copy tensors
-        into CUDA pinned memory before returning them. (the default is False)
+        If ``True``, the data loader will copy tensors into CUDA pinned memory
+        before returning them. (the default is False)
     collate_fn : callable, optional
         Merges a list of samples to form a mini-batch. (the default is None)
     transform : callable, optional
-        Transforms row of :attr:`meta_data` (the default is None)
+        Transforms row of :attr:`meta_data`. (the default is None)
     sampler : Sampler, optional
-        Defines the strategy to draw samples from
-        the dataset. If specified, ``shuffle`` must be False. (the default is None)
+        Defines the strategy to draw samples from the dataset. If specified,
+        ``shuffle`` must be False. (the default is None)
     batch_sampler : callable, optional
-        like sampler, but returns a batch of
-        indices at a time. Mutually exclusive with :attr:`batch_size`,
-        :attr:`shuffle`, :attr:`sampler`, and :attr:`drop_last`. (the default is None)
+        Like sampler, but returns a batch of indices at a time.
+        Mutually exclusive with :attr:`batch_size`, :attr:`shuffle`,
+        :attr:`sampler`, and :attr:`drop_last`. (the default is None)
     drop_last : bool, optional
-        set to ``True`` to drop the last incomplete batch,
+        Set to ``True`` to drop the last incomplete batch,
         if the dataset size is not divisible by the batch size. If ``False`` and
         the size of dataset is not divisible by the batch size, then the last batch
         will be smaller. (the default is True)
     timeout : int, optional
-        if positive, the timeout value for collecting a batch
-        from workers. If ``0``, ignore ``timeout`` notion. Should always be non-negative. (the default is 0)
+        If positive, the timeout value for collecting a batch from workers.
+        If ``0``, ignores ``timeout`` notion. Must be non-negative. (the default is 0)
     """
 
     def __init__(self, root: str, meta_data: pd.DataFrame, parse_item_cb: callable, batch_size: int = 1,
@@ -76,25 +74,22 @@ class ItemLoader(object):
         self.__iter_loader = iter(self.data_loader)
         
     def __len__(self):
-        """ Get length of teh dataloader
-
+        """ Get length of teh dataloader.
         """
         return len(self.data_loader)
 
     def sample(self, k=1):
         """Sample one or more mini-batches.
 
-        if not
-        
         Parameters
         ----------
         k : int, optional
-            The number of batches to sample (the default is 1)
+            The number of batches to sample. (the default is 1)
         
         Returns
         -------
-        list
-            List of sampled batches
+        samples : list
+            List of sampled batches.
         """
         samples = []
         for i in range(k):
@@ -106,5 +101,5 @@ class ItemLoader(object):
                 batch = next(self.__iter_loader)
 
             samples.append(batch)
-            
+
         return samples
