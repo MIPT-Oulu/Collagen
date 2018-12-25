0af5e491fa9b228c3e750d911026dd772d8579be
lext <aleksei.tiulpin@protonmail.ch>
2018-12-25 19:36:13 +0200

    Wrote a test to ensure correctness of drop_last working.

--- tests/test_itemloader.py
+++ tests/test_itemloader.py
@@ -40,3 +40,21 @@ def test_loader_endless_sampling_works(batch_size, n_samples, metadata_fname_tar
         assert len(samples) == n_samples
         assert samples[0]['img'].size(0) == batch_size
         assert samples[0]['target'].size(0) == batch_size
+
+
+@pytest.mark.parametrize('batch_size, n_samples, drop_last', itertools.product([3, 32], [1, 2], [True, False]))
+def test_loader_drop_last(batch_size, n_samples, metadata_fname_target_5_classes,
+                          ones_image_parser, img_target_transformer, drop_last):
+
+    itermloader = ItemLoader('/tmp/', meta_data=metadata_fname_target_5_classes,
+                             batch_size=batch_size, parse_item_cb=ones_image_parser,
+                             transform=img_target_transformer, shuffle=True, drop_last=drop_last)
+
+    if drop_last:
+        assert len(itermloader) == metadata_fname_target_5_classes.shape[0] // batch_size
+    else:
+        if metadata_fname_target_5_classes.shape[0] % batch_size != 0:
+            assert len(itermloader) == metadata_fname_target_5_classes.shape[0] // batch_size + 1
+        else:
+            assert len(itermloader) == metadata_fname_target_5_classes.shape[0] // batch_size
+
