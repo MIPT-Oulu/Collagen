75d9354c4479f7f10b3c170b479eb3d2dc7765e1
lext <aleksei.tiulpin@protonmail.ch>
2018-12-28 16:27:02 +0200

    device added

--- examples/minist_classification/train_mnist.py
+++ examples/minist_classification/train_mnist.py
@@ -1,4 +1,7 @@
 import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 from sklearn.model_selection import StratifiedShuffleSplit
 
 from tqdm import tqdm
@@ -25,16 +28,17 @@ if __name__ == "__main__":
     for fold_id, (x_train, x_val) in enumerate(FoldSplit(train_ds, random_state=args.seed)):
         item_loaders = dict()
 
+        kwargs = {'batch_size': args.bs, 'num_workers': args.num_threads}
         for stage, ds, trf in zip(['train', 'eval'], [x_train, x_val], init_mnist_transforms()):
             item_loaders[f'{fold_id}_{stage}'] = ItemLoader(meta_data=ds,
                                                             transform=trf, parse_item_cb=parse_item_mnist,
-                                                            batch_size=args.bs, num_workers=args.num_threads)
+                                                            **kwargs)
 
         data_provider = DataProvider(item_loaders)
 
-        model = SimpleConvNet(bw=args.bw, drop=args.dropout, n_cls=len(classes))
+        model = SimpleConvNet(bw=args.bw, drop=args.dropout, n_cls=len(classes)).to(device)
         optimizer = torch.optim.Adam(params=model.parameters(), lr=args.lr, weight_decay=args.wd)
-        criterion = torch.nn.CrossEntropyLoss()
+        criterion = torch.nn.CrossEntropyLoss().to(device)
 
         se = Session(module=model, optimizer=optimizer, loss=criterion)
         st = TrainValStrategy(data_provider, f'{fold_id}_train', f'{fold_id}_eval', se)
