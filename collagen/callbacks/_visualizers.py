Add kappa score Fixed confusion matrix with normalization

collagen/callbacks/_visualizers.py
@@ -12,9 +12,10 @@ from collagen.metrics import plot_confusion_matrix
 
 
 class ConfusionMatrixVisualizer(Callback):
-    def __init__(self, writer, labels: list or None = None, tag="confusion_matrix"):
+    def __init__(self, writer, labels: list or None = None, tag="confusion_matrix", normalize = False):
         super().__init__(type="visualizer")
         self._labels = labels
+        self._normalize = normalize
         self.__epoch = 0
         self._writer = writer
         self._tag = tag
@@ -48,7 +49,7 @@ class ConfusionMatrixVisualizer(Callback):
         self._predicts += [self._labels[i] for i in to_cpu(decoded_pred_cls, use_numpy=True).tolist()]
 
     def on_epoch_end(self, *args, **kwargs):
-        fig = plot_confusion_matrix(np.array(self._corrects), np.array(self._predicts), labels=self._labels)
+        fig = plot_confusion_matrix(np.array(self._corrects), np.array(self._predicts), labels=self._labels, normalize=self._normalize)
         self._writer.add_figure(self._tag, fig, global_step=self.__epoch)
 
 

collagen/metrics/__init__.py
@@ -5,18 +5,19 @@ from matplotlib import figure
 from sklearn.metrics import confusion_matrix
 import numpy as np
 from ._meters import Meter, RunningAverageMeter, AccuracyMeter, AccuracyThresholdMeter, SSAccuracyMeter, SSValidityMeter
+from ._meters import KappaMeter
 
 
 def plot_confusion_matrix(correct_labels, predict_labels, labels, normalize=False):
     cm = confusion_matrix(correct_labels, predict_labels, labels=labels)
     if normalize:
-        cm = cm.astype('float')*10 / cm.sum(axis=1)[:, np.newaxis]
+        cm = cm.astype('float')*100 / cm.sum(axis=1)[:, np.newaxis]
         cm = np.nan_to_num(cm, copy=True)
         cm = cm.astype('int')
 
     np.set_printoptions(precision=2)
 
-    fig = figure.Figure(figsize=(7, 7), dpi=320, facecolor='w', edgecolor='k')
+    fig = figure.Figure(figsize=(5, 5), dpi=230, facecolor='w', edgecolor='k')
     ax = fig.add_subplot(1, 1, 1)
     im = ax.imshow(cm, cmap='Oranges')
 