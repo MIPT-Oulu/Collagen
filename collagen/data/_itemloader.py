5822bdff918daa936fc5ba79d760275c1cd871d7
hoang <nguyenhuyhoang83@gmail.com>
2019-06-14 17:18:02 +0300

    Update MixUpSampler 1 and 2

--- collagen/data/_itemloader.py
+++ collagen/data/_itemloader.py
@@ -131,9 +131,89 @@ class ItemLoader(object):
 
 class MixUpSampler(ItemLoader):
     def __init__(self, name: str, alpha: callable or float, model: nn.Module or None = None,
-                 data_rearrage: callable or None = None, target_rearrage: callable or None = None,
-                 data_key: str = "data", target_key: str = 'target',
+                 data_rearrange: callable or None = None, target_rearrage: callable or None = None,
+                 data_key: str = "data", target_key: str = 'target', min_lambda: float = 0.7,
                  parse_item_cb: callable or None = None, meta_data: pd.DataFrame or None = None,
+                 root: str or None = None, batch_size: int = 1, num_workers: int = 0, shuffle: bool = False,
+                 pin_memory: bool = False, collate_fn: callable = default_collate, transform: callable or None = None,
+                 sampler: torch.utils.data.sampler.Sampler or None = None, batch_sampler=None, compute_logits_mixup:bool = True,
+                 drop_last: bool = False, timeout: int = 0, detach: bool = False):
+        super().__init__(meta_data=meta_data, parse_item_cb=parse_item_cb, root=root, batch_size=batch_size,
+                         num_workers=num_workers, shuffle=shuffle, pin_memory=pin_memory, collate_fn=collate_fn,
+                         transform=transform, sampler=sampler, batch_sampler=batch_sampler, drop_last=drop_last, timeout=timeout)
+
+        self.__model = model
+        self.__name = name
+        self.__data_key = data_key
+        self.__target_key = target_key
+        self.__alpha = alpha
+        self.__data_rearrange = self._default_change_data_ordering if data_rearrange is None else data_rearrange
+        self.__min_l = min_lambda
+        self.__compute_logits_mixup = compute_logits_mixup
+
+    @property
+    def alpha(self):
+        return self.__alpha
+
+    @staticmethod
+    def _default_change_data_ordering(x, y):
+        return torch.flip(x, dims=[0]), torch.flip(y, dims=[0])
+
+    def __len__(self):
+        return super().__len__()
+
+    def sample(self, k=1):
+        sampled_rows = super().sample(k)
+        device = next(self.__model.parameters()).device
+        samples = []
+        for i in range(k):
+            imgs1 = sampled_rows[i][self.__data_key]
+            target1 = sampled_rows[i][self.__target_key]
+
+            imgs2, target2 = self.__data_rearrange(imgs1, target1)
+
+            if callable(self.alpha):
+                l = self.alpha()
+            elif isinstance(self.alpha, float):
+                l = self.alpha
+            else:
+                raise ValueError('Not support alpha of {}'.format(type(self.alpha)))
+
+            if not isinstance(l, float) or l < 0 or l > 1:
+                raise ValueError('Alpha {} is not float or out of range [0,1]'.format(l))
+            elif l < 0.5:
+                l = 1 - l
+
+            l = max(l, self.__min_l)
+
+            mixup_imgs = l * imgs1 + (1 - l) * imgs2
+
+            logits1 = self.__model(imgs1.to(device))
+            logits2 = self.__model(imgs2.to(device))
+
+            if self.__compute_logits_mixup:
+                logits_mixup = l * logits1 + (1 - l) * logits2
+            else:
+                logits_mixup = None
+
+            # onehot1 = torch.zeros([batch_size, self.__n_classes])
+            # onehot2 = torch.zeros([batch_size, self.__n_classes])
+
+            # batch_size = imgs1.shape[0]
+            # onehot1.scatter_(1, target1.type(torch.int64).unsqueeze(-1), 1.0)
+            # onehot2.scatter_(1, target2.type(torch.int64).unsqueeze(-1), 1.0)
+            # mixup_target = l*onehot1 + (1 - l)*onehot2
+
+            samples.append({'name': self.__name, 'mixup_data': mixup_imgs, 'target': target1, 'target_bg': target2,
+                            'alpha': l, 'logits_mixup': logits_mixup})
+        return samples
+
+
+class MixUpSampler2(ItemLoader):
+    def __init__(self, name: str, alpha: callable or float, model: nn.Module or None = None,
+                 data_rearrange: callable or None = None, target_rearrage: callable or None = None,
+                 data_key: str = "data", target_key: str = 'target', augmentation: callable or None = None,
+                 parse_item_cb: callable or None = None, meta_data: pd.DataFrame or None = None, min_lambda: float = 0.7,
                  root: str or None = None, batch_size: int = 1, num_workers: int = 0, shuffle: bool = False,
                  pin_memory: bool = False, collate_fn: callable = default_collate, transform: callable or None = None,
                  sampler: torch.utils.data.sampler.Sampler or None = None, batch_sampler=None,
@@ -148,20 +228,18 @@ class MixUpSampler(ItemLoader):
         self.__data_key = data_key
         self.__target_key = target_key
         self.__alpha = alpha
-        self.__data_rearrage = self._default_change_data_ordering if data_rearrage is None else data_rearrage
-        self.__target_rearrage = self._default_change_target_ordering if target_rearrage is None else target_rearrage
+        self.__data_rearrange = self._default_change_data_ordering if data_rearrange is None else data_rearrange
+
+        self.__augmentation = augmentation
+        self.__min_l = min_lambda
 
     @property
     def alpha(self):
         return self.__alpha
 
     @staticmethod
-    def _default_change_data_ordering(x):
-        return torch.flip(x, dims=[0])
-
-    @staticmethod
-    def _default_change_target_ordering(y):
-        return torch.flip(y, dims=[0])
+    def _default_change_data_ordering(x, y):
+        return torch.flip(x, dims=[0]), torch.flip(y, dims=[0])
 
     def __len__(self):
         return super().__len__()
@@ -174,8 +252,8 @@ class MixUpSampler(ItemLoader):
             imgs1 = sampled_rows[i][self.__data_key]
             target1 = sampled_rows[i][self.__target_key]
 
-            imgs2 = self.__data_rearrage(imgs1)
-            target2 = self.__target_rearrage(target1)
+            imgs2, target2 = self.__data_rearrange(imgs1, target1)
+
             if callable(self.alpha):
                 l = self.alpha()
             elif isinstance(self.alpha, float):
@@ -188,9 +266,20 @@ class MixUpSampler(ItemLoader):
             elif l < 0.5:
                 l = 1 - l
 
-            if self.__model is not None:
-                logits1 = self.__model(imgs1.to(device))
-                logits2 = self.__model(imgs2.to(device))
+            l = max(l, self.__min_l)
+
+            mixup_imgs = l * imgs1 + (1 - l) * imgs2
+
+            logits1 = self.__model(imgs1.to(device))
+            logits2 = self.__model(imgs2.to(device))
+            # mixup_logits = self.__model(mixup_imgs.to(device))
+
+            imgs1_cpu = to_cpu(imgs1.permute(0, 2, 3, 1), use_numpy=True)
+            imgs1_aug = self.__augmentation(imgs1_cpu)
+            logits1_aug = self.__model(imgs1_aug.to(device))
+
+            # mixup_imgs_cpu = to_cpu(mixup_imgs.permute(0, 2, 3, 1), use_numpy=True)
+            # mixup_aug_imgs = self.__augmentation(mixup_imgs_cpu)
 
             # onehot1 = torch.zeros([batch_size, self.__n_classes])
             # onehot2 = torch.zeros([batch_size, self.__n_classes])
@@ -200,10 +289,11 @@ class MixUpSampler(ItemLoader):
             # onehot2.scatter_(1, target2.type(torch.int64).unsqueeze(-1), 1.0)
             # mixup_target = l*onehot1 + (1 - l)*onehot2
 
-            mixup_imgs = l*imgs1 + (1 - l)*imgs2
-            mixup_logits = l*logits1 + (1 - l)*logits2
-            samples.append({'name': self.__name, 'mixup_data': mixup_imgs, 'target': target1, 'target_bg': target2,
-                            'alpha': l, 'mixup_logits': mixup_logits})
+            logits_mixup = l*logits1 + (1 - l)*logits2
+            samples.append({'name': self.__name, 'mixup_data': mixup_imgs,
+                            'target': target1, 'target_bg': target2,
+                            'logits': logits1, 'logits_aug': logits1_aug,
+                            'logits_mixup': logits_mixup, 'alpha': l})
         return samples
 
 
@@ -341,9 +431,6 @@ class AugmentedGroupSampler2(ItemLoader):
                 raise ValueError('Empty list!')
 
             te_f = te_f.detach()
-            # if self.__detach:
-                # logits = logits.detach()
-                # f = f.detach()
 
             samples.append({'name': self.__name, 'st_features': st_f, 'te_features': te_f, 'data': imgs, 'target': target})
         return samples
