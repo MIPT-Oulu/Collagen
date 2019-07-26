b777ae323655702a35ae3857d09a4ff4235f1d82
lext <aleksei.tiulpin@protonmail.ch>
2019-07-26 15:04:10 +0300

    Confusion meter test

--- tests/test_meters.py
+++ tests/test_meters.py
@@ -14,10 +14,16 @@ def test_confusion_meter(classification_problem_output, batch_size):
 
     size = gt.size(0)
     n_batches = size // batch_size
-    for i in range(n_batches+1):
-        batch_gt = gt[i*batch_size:i*batch_size+batch_size]
-        batch_preds = preds[i * batch_size:i * batch_size + batch_size, :]
+    for epoch in range(2):
+        meter.on_epoch_begin()
+        # Making sure that the meter resets
+        np.testing.assert_equal(np.zeros((n_classes, n_classes), dtype=np.uint64),
+                                meter.current())
 
-        meter.on_minibatch_end(batch_gt, batch_preds)
+        for i in range(n_batches+1):
+            batch_gt = gt[i*batch_size:i*batch_size+batch_size]
+            batch_preds = preds[i * batch_size:i * batch_size + batch_size, :]
 
+            meter.on_minibatch_end(batch_gt, batch_preds)
+    # Comparing to the sklearn implementation
     np.testing.assert_equal(cm, meter.current())
