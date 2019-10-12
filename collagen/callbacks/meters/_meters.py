2fcf62b286504d829d8a75a7391451b6fc2fcb74
lext <aleksei.tiulpin@protonmail.ch>
2019-10-12 21:23:31 +0300

    fixed accuracy threshold meter

--- collagen/callbacks/meters/_meters.py
+++ collagen/callbacks/meters/_meters.py
@@ -189,20 +189,34 @@ class BalancedAccuracyMeter(Meter):
 
 
 class AccuracyThresholdMeter(Meter):
-    def __init__(self, name: str = "binary_accuracy", threshold: float = 0.5, sigmoid: bool = False, prefix=""):
+    def __init__(self, name: str = "binary_accuracy", threshold: float = 0.5,
+                 sigmoid: bool = False, prefix="",
+                 parse_target=None, parse_output=None, batch_wise=False):
         super().__init__(name=name, prefix=prefix)
+        self.__batch_wise = batch_wise
         self.__threshold: float = threshold
         self.__sigmoid: bool = sigmoid
         self.__data_count = 0.0
         self.__correct_count = 0.0
         self.__accuracy = None
 
+        self.__parse_target = Meter.default_parse_target if parse_target is None else parse_target
+        self.__parse_output = Meter.default_parse_output if parse_output is None else parse_output
+
     def on_epoch_begin(self, epoch, *args, **kwargs):
         self.__data_count = 0.0
         self.__correct_count = 0.0
 
+    def on_minibatch_begin(self, *args, **kwargs):
+        if self.__batch_wise:
+            self.__data_count = 0.0
+            self.__correct_count = 0.0
+
     def on_minibatch_end(self, target, output, device=None, **kwargs):
         n = target.shape[0]
+
+        target = self.__parse_target(target)
+        output = self.__parse_output(output)
 
         if device is None:
             device = output.device
@@ -215,7 +229,7 @@ class AccuracyThresholdMeter(Meter):
         if self.__sigmoid:
             output_on_device = output_on_device.sigmoid()
 
-        self.__correct_count += ((output_on_device > self.__threshold) == target_on_device.byte()).float().sum()
+        self.__correct_count += ((output_on_device > self.__threshold) == target_on_device.bool()).float().sum()
         self.__data_count += n
 
     def on_epoch_end(self, epoch, n_epochs, *args, **kwargs):
