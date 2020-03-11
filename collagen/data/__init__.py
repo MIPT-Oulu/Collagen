64ceda38a85b891d391ffbc84e108424b76e3535
Hoang <nguyenhuyhoang83@gmail.com>
2020-03-11 21:03:51 +0200

    Clean up

--- collagen/data/__init__.py
+++ collagen/data/__init__.py
@@ -3,6 +3,5 @@ from ._dataset import DataFrameDataset
 from ._itemloader import ItemLoader
 from ._splitter import Splitter, FoldSplit, TrainValSplit, SSFoldSplit
 from ._distributed_itemloader import DistributedItemLoader
-from .datasets import *
 from .data_provider import *
 from .utils import *
